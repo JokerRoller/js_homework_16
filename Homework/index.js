c29d267793bfa0fe0c3162105e1cd0ed415cc10c
JokerRoller <BECOOL686868@gmail.com>
2022-06-14 18:33:34 +0400

    asd

--- Homework/index.js
+++ Homework/index.js
@@ -1,3 +1,22 @@
-var move = document.getElementById('fa-star');
-move.style.transform = "translateX(270px)";
-move.style.transition = "all 2s"
+const star = document.getElementById('fa-star');
+let roll = 0;
+const btn = document.getElementById('roll');
+btn.addEventListener('click', move);
+btn.addEventListener('click', randomizer);
+
+function randomizer() {
+  var dice = (Math.floor(Math.random()*9)) + 1;
+  roll = dice;
+}
+
+
+function move () {
+    for (let i = 0; i < roll; i++ ) {
+    star.style.transform = `translateX(${roll*90}px)`;
+    star.style.transition = "all 2s";
+    document.getElementById('roll').innerHTML = `${roll}`;
+}
+}
+
+
+
